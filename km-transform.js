ff0fbb1664ac00cdfc3e361b8621b9ab2d5c0605
Kyle Meenehan <kylemeenehan@gmail.com>
2018-02-20 15:24:19 +0200

    added script

---
+++ km-transform.js
@@ -0,0 +1,22 @@
+Object.prototype.transform = function(callback) {
+
+  var T, A;
+
+  if (this == null) {
+    throw new TypeError('this is null or not defined');
+  }
+
+  var O = Object(this);
+
+  if (typeof callback!== 'function') {
+    throw new TypeError(callback + ' is not a function');
+  }
+
+  if (arguments.length > 1) {
+    T = arguments[1];
+  }
+
+  transformedObject = callback.call(T, O);
+
+  return transformedObject;
+}
